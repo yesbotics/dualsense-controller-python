ce518c423393b2037d72fe9df67f107387a0c903
Albrecht Nitsche <albrecht.nitsche@bibliothek.uni-halle.de>
2023-07-17 13:19:09 +0200

    poetry buildability

--- dualsense_controller/__init__.py
+++ dualsense_controller/__init__.py
@@ -1,1 +1,1 @@
-from .example import *
+from .DualSenseController import *
--- dualsense_controller/example.py
+++ dualsense_controller/example.py
@@ -1,5 +1,9 @@
+from dualsense_controller import DualSenseController
+
+
 def main():
     print("main")
+    dsc: DualSenseController = DualSenseController()
 
 
 if __name__ == "__main__":
