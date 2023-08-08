8badd76e43abeed24083b5380f8e4f2bc8cbb177
Albrecht Nitsche <albrechtnitsche@gmail.com>
2023-08-08 13:08:12 +0200

    use conftest for fixtures

--- tests/test_DualSenseController.py
+++ tests/test_DualSenseController.py
@@ -1,100 +1,15 @@
-from dataclasses import dataclass
-from typing import Generator
-from unittest.mock import MagicMock, Mock, patch
+from unittest.mock import MagicMock
 
 import pytest as pytest
-from _pytest.fixtures import SubRequest
-from pytest_mock import MockerFixture
 
-from dualsense_controller import ConnectionType, DualSenseController, InvalidDeviceIndexException, Mapping, Number, \
-    UpdateLevel
-from tests.mock import ConnTypeMock, DeviceInfoMock, DeviceMock
-
-
-@dataclass
-class ControllerInstanceData:
-    controller: DualSenseController
-    device: DeviceMock
-
-
-@pytest.fixture
-def device_info_mock() -> DeviceInfoMock:
-    return DeviceInfoMock()
-
-
-@pytest.fixture
-def enumerate_devices_mock(
-        request: SubRequest,
-        device_info_mock: DeviceInfoMock
-) -> Generator[MagicMock, None, None]:
-    num_infos: int = request.param if hasattr(request, 'param') else 1
-    with patch(
-            "dualsense_controller.core.HidControllerDevice.HidControllerDevice.enumerate_devices",
-            return_value=([device_info_mock] * num_infos) if num_infos > 0 else []
-    ) as enumerate_devices_mock:
-        yield enumerate_devices_mock
-
-
-@pytest.fixture
-def controller_instance(
-        request: SubRequest,
-        enumerate_devices_mock: MagicMock,
-        mocker: MockerFixture
-) -> Generator[ControllerInstanceData, None, None]:
-    params: tuple[ConnTypeMock, Mapping, UpdateLevel] = request.param if hasattr(request, 'param') else ()
-    param_conn_type: ConnTypeMock = params[0] if len(params) >= 1 else ConnTypeMock.USB_01
-    param_mapping: Mapping = params[1] if len(params) >= 2 else Mapping.DEFAULT
-    param_update_level: UpdateLevel = params[2] if len(params) >= 3 else UpdateLevel.DEFAULT
-
-    device_mock: DeviceMock = DeviceMock(conn_type=param_conn_type)
-    create_mock: Mock = mocker.patch(
-        'dualsense_controller.core.HidControllerDevice.HidControllerDevice._create'
-    )
-    create_mock.return_value = device_mock
-
-    yield ControllerInstanceData(
-        controller=DualSenseController(
-            device_index_or_device_info=enumerate_devices_mock,
-            mapping=param_mapping,
-            update_level=param_update_level,
-        ),
-        device=device_mock
-    )
-
-
-@pytest.fixture
-def activated_instance(
-        request: SubRequest,
-        enumerate_devices_mock: MagicMock,
-        mocker: MockerFixture
-):
-    params: tuple[ConnTypeMock, Mapping, UpdateLevel] = request.param if hasattr(request, 'param') else ()
-    param_conn_type: ConnTypeMock = params[0] if len(params) >= 1 else ConnTypeMock.USB_01
-    param_mapping: Mapping = params[1] if len(params) >= 2 else Mapping.DEFAULT
-    param_update_level: UpdateLevel = params[2] if len(params) >= 3 else UpdateLevel.DEFAULT
-
-    device_mock: DeviceMock = DeviceMock(conn_type=param_conn_type)
-    create_mock: Mock = mocker.patch(
-        'dualsense_controller.core.HidControllerDevice.HidControllerDevice._create'
-    )
-    create_mock.return_value = device_mock
-
-    controller: DualSenseController = DualSenseController(
-        device_index_or_device_info=enumerate_devices_mock,
-        mapping=param_mapping,
-        update_level=param_update_level,
-    )
-    controller.activate()
-    yield ControllerInstanceData(
-        controller=controller,
-        device=device_mock
-    )
-    controller.deactivate()
+from dualsense_controller import ConnectionType, DualSenseController, InvalidDeviceIndexException, Mapping, Number
+from tests.common import ControllerInstanceData, ControllerInstanceParams
+from tests.mock.common import ConnTypeMock
 
 
 # # @pytest.mark.skip(reason="temp disabled")
 @pytest.mark.parametrize(
-    'enumerate_devices_mock,device_index,expect_is_raising_exception',
+    'fixture_enumerate_devices_mock,device_index,expect_is_raising_exception',
     [
         [0, None, True],
         [0, 0, True],
@@ -119,10 +34,10 @@ def activated_instance(
         [3, 4, True],
         [4, 999, True],
     ],
-    indirect=['enumerate_devices_mock']
+    indirect=['fixture_enumerate_devices_mock']
 )
 def test_device_index(
-        enumerate_devices_mock: MagicMock,
+        fixture_enumerate_devices_mock: MagicMock,
         device_index: int,
         expect_is_raising_exception: bool
 ) -> None:
@@ -135,80 +50,80 @@ def test_device_index(
 
 # @pytest.mark.skip(reason="temp disabled")
 @pytest.mark.parametrize(
-    'controller_instance',
+    'fixture_controller_instance',
     [
-        [ConnTypeMock.USB_01],
-        [ConnTypeMock.BT_31],
+        ControllerInstanceParams(ConnTypeMock.USB_01),
+        ControllerInstanceParams(ConnTypeMock.BT_31),
     ],
-    indirect=['controller_instance']
+    indirect=['fixture_controller_instance']
 )
-def test_instance(controller_instance: ControllerInstanceData) -> None:
-    assert isinstance(controller_instance.controller, DualSenseController)
+def test_instance(fixture_controller_instance: ControllerInstanceData) -> None:
+    assert isinstance(fixture_controller_instance.controller, DualSenseController)
 
 
 # @pytest.mark.skip(reason="temp disabled")
 @pytest.mark.parametrize(
-    'controller_instance,expected_conn_type',
+    'fixture_controller_instance,expected_conn_type',
     [
-        [[ConnTypeMock.USB_01], ConnectionType.USB_01],
-        [[ConnTypeMock.BT_31], ConnectionType.BT_31],
+        [ControllerInstanceParams(ConnTypeMock.USB_01), ConnectionType.USB_01],
+        [ControllerInstanceParams(ConnTypeMock.BT_31), ConnectionType.BT_31],
     ],
-    indirect=['controller_instance']
+    indirect=['fixture_controller_instance']
 )
 def test_instance_conntype(
-        controller_instance: ControllerInstanceData,
+        fixture_controller_instance: ControllerInstanceData,
         expected_conn_type: ConnectionType
 ) -> None:
     try:
-        controller_instance.controller.activate()
-        assert isinstance(controller_instance.controller, DualSenseController)
-        assert controller_instance.controller.is_active
-        assert controller_instance.controller.connection_type.name == expected_conn_type.name
+        fixture_controller_instance.controller.activate()
+        assert isinstance(fixture_controller_instance.controller, DualSenseController)
+        assert fixture_controller_instance.controller.is_active
+        assert fixture_controller_instance.controller.connection_type.name == expected_conn_type.name
     finally:
-        controller_instance.controller.deactivate()
+        fixture_controller_instance.controller.deactivate()
 
 
 # @pytest.mark.skip(reason="temp disabled")
 @pytest.mark.parametrize(
-    'activated_instance,left_stick_y_raw,left_stick_y_mapped',
+    'fixture_activated_instance,left_stick_y_raw,left_stick_y_mapped',
     [
-        [[ConnTypeMock.USB_01, Mapping.RAW], 0, 0],
-        [[ConnTypeMock.USB_01, Mapping.RAW], 127, 127],
-        [[ConnTypeMock.USB_01, Mapping.RAW], 128, 128],
-        [[ConnTypeMock.USB_01, Mapping.RAW], 255, 255],
-        [[ConnTypeMock.USB_01, Mapping.RAW_INVERTED], 0, 255],
-        [[ConnTypeMock.USB_01, Mapping.RAW_INVERTED], 127, 128],
-        [[ConnTypeMock.USB_01, Mapping.RAW_INVERTED], 128, 127],
-        [[ConnTypeMock.USB_01, Mapping.RAW_INVERTED], 255, 0],
-        [[ConnTypeMock.USB_01, Mapping.DEFAULT], 0, 127],
-        [[ConnTypeMock.USB_01, Mapping.DEFAULT], 127, 0],
-        [[ConnTypeMock.USB_01, Mapping.DEFAULT], 128, -1],
-        [[ConnTypeMock.USB_01, Mapping.DEFAULT], 255, -128],
-        [[ConnTypeMock.USB_01, Mapping.DEFAULT_INVERTED], 0, -128],
-        [[ConnTypeMock.USB_01, Mapping.DEFAULT_INVERTED], 127, -1],
-        [[ConnTypeMock.USB_01, Mapping.DEFAULT_INVERTED], 128, 0],
-        [[ConnTypeMock.USB_01, Mapping.DEFAULT_INVERTED], 255, 127],
-        [[ConnTypeMock.USB_01, Mapping.NORMALIZED], 0, 1.0],
-        [[ConnTypeMock.USB_01, Mapping.NORMALIZED], 127, 0.0],
-        [[ConnTypeMock.USB_01, Mapping.NORMALIZED], 128, 0.0],
-        [[ConnTypeMock.USB_01, Mapping.NORMALIZED], 255, -1.0],
-        [[ConnTypeMock.USB_01, Mapping.NORMALIZED_INVERTED], 0, -1.0],
-        [[ConnTypeMock.USB_01, Mapping.NORMALIZED_INVERTED], 127, 0.0],
-        [[ConnTypeMock.USB_01, Mapping.NORMALIZED_INVERTED], 128, 0.0],
-        [[ConnTypeMock.USB_01, Mapping.NORMALIZED_INVERTED], 255, 1.00],
-        [[ConnTypeMock.USB_01, Mapping.HUNDRED], 0, 100],
-        [[ConnTypeMock.USB_01, Mapping.HUNDRED], 127, 0],
-        [[ConnTypeMock.USB_01, Mapping.HUNDRED], 128, 0],
-        [[ConnTypeMock.USB_01, Mapping.HUNDRED], 255, -100],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.RAW), 0, 0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.RAW), 127, 127],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.RAW), 128, 128],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.RAW), 255, 255],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.RAW_INVERTED), 0, 255],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.RAW_INVERTED), 127, 128],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.RAW_INVERTED), 128, 127],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.RAW_INVERTED), 255, 0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.DEFAULT), 0, 127],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.DEFAULT), 127, 0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.DEFAULT), 128, -1],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.DEFAULT), 255, -128],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.DEFAULT_INVERTED), 0, -128],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.DEFAULT_INVERTED), 127, -1],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.DEFAULT_INVERTED), 128, 0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.DEFAULT_INVERTED), 255, 127],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.NORMALIZED), 0, 1.0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.NORMALIZED), 127, 0.0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.NORMALIZED), 128, 0.0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.NORMALIZED), 255, -1.0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.NORMALIZED_INVERTED), 0, -1.0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.NORMALIZED_INVERTED), 127, 0.0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.NORMALIZED_INVERTED), 128, 0.0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.NORMALIZED_INVERTED), 255, 1.00],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.HUNDRED), 0, 100],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.HUNDRED), 127, 0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.HUNDRED), 128, 0],
+        [ControllerInstanceParams(ConnTypeMock.USB_01, Mapping.HUNDRED), 255, -100],
     ],
-    indirect=['activated_instance']
+    indirect=['fixture_activated_instance']
 )
 def test_mapping(
-        activated_instance: ControllerInstanceData,
+        fixture_activated_instance: ControllerInstanceData,
         left_stick_y_raw: int,
         left_stick_y_mapped: Number,
 ) -> None:
-    activated_instance.device.set_left_stick_y_byte(left_stick_y_raw)
-    activated_instance.controller.wait_until_updated()
-    assert isinstance(activated_instance.controller, DualSenseController)
-    assert left_stick_y_mapped == pytest.approx(activated_instance.controller.left_stick_y.value, rel=1e-2)
+    fixture_activated_instance.hidapi_device.set_left_stick_y_byte(left_stick_y_raw)
+    fixture_activated_instance.controller.wait_until_updated()
+    assert isinstance(fixture_activated_instance.controller, DualSenseController)
+    assert left_stick_y_mapped == pytest.approx(fixture_activated_instance.controller.left_stick_y.value, rel=1e-2)
