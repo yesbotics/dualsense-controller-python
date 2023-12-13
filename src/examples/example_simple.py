bd2e5fc92a00736c8198d8e99b07266764f6ec88
Albrecht Nitsche <>
2023-12-13 11:37:46 +0100

    add another example

--- src/examples/example_simple.py
+++ src/examples/example_simple.py
@@ -1,30 +1,68 @@
-from dualsense_controller import DualSenseController, Mapping
+from time import sleep
 
+from dualsense_controller import DualSenseController
 
-def on_button_pressed():
-    pass
-
-
+# list availabe devices and throw exception when tzhere is no device detected
 device_infos = DualSenseController.enumerate_devices()
 if len(device_infos) < 1:
     raise Exception('No DualSense Controller available.')
 
-controller = DualSenseController(mapping=Mapping.NORMALIZED)
-controller.activate()
+# flag, which keeps program alive
+is_running = True
+
+# create an instance, use fiŕst available device
+controller = DualSenseController()
 
 
+# switches the keep alive flag, which stops the below loop
+def stop():
+    global is_running
+    is_running = False
+
+
+# callback, when cross button is pressed, which enables rumble
 def on_cross_btn_pressed():
     print('cross button pressed')
+    controller.left_rumble.set(255)
+    controller.right_rumble.set(255)
 
 
+# callback, when cross button is released, which disables rumble
 def on_cross_btn_released():
-    print('cross button_released')
+    print('cross button released')
+    controller.left_rumble.set(0)
+    controller.right_rumble.set(0)
 
 
-def on_cross_btn_changed(pressed):
-    print(f'cross button is pressed: {pressed}')
+# callback, when PlayStation button is pressed
+# stop program
+def on_ps_btn_pressed():
+    print('PS button released -> stop')
+    stop()
 
 
+# callback, when unintended error occurs,
+# i.e. physically disconnecting the controller during operation
+# stop program
+def on_error(error):
+    print(f'Opps! an error occured: {error}')
+    stop()
+
+
+# register the button callbacks
 controller.btn_cross.on_down(on_cross_btn_pressed)
 controller.btn_cross.on_up(on_cross_btn_released)
-controller.btn_cross.on_change(on_cross_btn_changed)
+controller.btn_ps.on_down(on_ps_btn_pressed)
+
+# register the error callback
+controller.on_error(on_error)
+
+# enable/connect the device
+controller.activate()
+
+# start keep alive loop, controller inputs and callbacks are handled in a second thread
+while is_running:
+    sleep(0.001)
+
+# disable/disconnect controller device
+controller.deactivate()
